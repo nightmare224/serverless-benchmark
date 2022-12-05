5cd22942ebe0e771f9c434a18180bb251b4f1d2f
Nicole Tai <nicole1999224@gmail.com>
2022-12-05 13:21:39 +0100

    collect metrics every 1 seconds

--- roles/resource-metrics/files/collect-metrics.sh
+++ roles/resource-metrics/files/collect-metrics.sh
@@ -1,5 +1,5 @@
 APPNAME=$1
 OUTFILE=$2
 rm -f ${APPNAME}
-# docker stats --format 'table \t{{.Name}}\t{{.CPUPerc}}\t{{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}' --no-stream | ts '%Y/%m/%d %H:%M:%S'
-while true; do docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.NetIO}}|{{.BlockIO}}' --no-stream | ts '%Y/%m/%d %H:%M:%S' | grep k8s_${APPNAME} >> ${OUTFILE}; done &
+# while true; do docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.NetIO}}|{{.BlockIO}}' --no-stream | ts '%Y/%m/%d %H:%M:%S' | grep k8s_${APPNAME} >> ${OUTFILE}; done &
+docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.BlockIO}}' | ts | awk '/1/ && match($4, ".k8s_${APPNAME}.*")' >> ${OUTFILE} &
