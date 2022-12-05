1fdc0bcab9854e586a63ec7ad17ef52b706fcf0f
Nicole Tai <nicole1999224@gmail.com>
2022-12-05 13:30:10 +0100

    collect metrics awk variable

--- roles/resource-metrics/files/collect-metrics.sh
+++ roles/resource-metrics/files/collect-metrics.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 APPNAME=$1
 OUTFILE=$2
 rm -f ${APPNAME}
 # while true; do docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.NetIO}}|{{.BlockIO}}' --no-stream | ts '%Y/%m/%d %H:%M:%S' | grep k8s_${APPNAME} >> ${OUTFILE}; done &
-docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.BlockIO}}' | ts | awk '/1/ && match($4, ".k8s_${APPNAME}.*")' >> ${OUTFILE} &
+docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.BlockIO}}' | ts | awk -v appname=${APPNAME} '/1/ && match($4, ".k8s_"appname".*")
