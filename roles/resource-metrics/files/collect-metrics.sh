25f6d7fd436fce3382fbeeba8698ead0f4299239
Nicole Tai <nicole1999224@gmail.com>
2022-12-08 16:10:37 +0100

    change resource metric name

--- roles/resource-metrics/files/collect-metrics.sh
+++ roles/resource-metrics/files/collect-metrics.sh
@@ -3,4 +3,4 @@ APPNAME=$1
 OUTFILE=$2
 rm -f ${APPNAME}
 # while true; do docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.NetIO}}|{{.BlockIO}}' --no-stream | ts '%Y/%m/%d %H:%M:%S' | grep k8s_${APPNAME} >> ${OUTFILE}; done &
-docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.BlockIO}}' | ts '%Y/%m/%d %H:%M:%S' | awk -v appname=${APPNAME} '/1/ && match($3, ".k8s_"appname".*")' >> ${OUTFILE} &
+docker stats --format 'table |{{.Name}}|{{.CPUPerc}}|{{.MemPerc}}|{{.BlockIO}}' | ts '%Y/%m/%d %H:%M:%S' | awk -v appname=${APPNAME} '/1/ && match($3, ".k8s_"appname".*")' >> ${OUTFILE}-${USER} &
