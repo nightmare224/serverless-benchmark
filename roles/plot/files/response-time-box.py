f4ddcfdb28815d0989b21e28866c14a71d989d21
Nicole Tai <nicole1999224@gmail.com>
2022-12-12 21:17:01 +0100

    box plot

--- roles/plot/files/response-time-box.py
+++ roles/plot/files/response-time-box.py
@@ -1,17 +1,16 @@
 import plotly.graph_objects as go
 import os
 import plotly.express as px
-from statistics import mean, stdev
+from plotly.subplots import make_subplots
 from files import files
 from config import *
 
-
 task_name = input(f"Task Name (default task is {TASK_NAME}, press ENTER to skip): ")
 TASK_NAME = TASK_NAME if task_name == "" else task_name
-OUTPUT = f"{os.getcwd()}/result/{TASK_NAME}-responsetime.html"
+OUTPUT = f"{os.getcwd()}/result/pattern1/{TASK_NAME}-responsetime.html"
 
 
-def get_response_time_list(filename):
+def get_response_time_from_file(filename):
     duration_list = []
     with open(filename) as f:
         lines = f.readlines()
@@ -25,60 +24,88 @@ def get_response_time_list(filename):
 
     return duration_list
 
+def get_response_time_from_filelist(filename_list):
+    duration_list = []
+    for filename in filename_list:
+        with open(filename) as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line[:-1] if line[-1] == "\n" else line
+                rsp = line.split(" ")
+                if rsp[2] == "Wrote":
+                    # seconds
+                    duration = float(rsp[7][:-1])
+                    duration_list.append(duration)
 
-fig = go.Figure()
+    return duration_list
 
-# x1 = ['day 1', 'day 1', 'day 1', 'day 1', 'day 1', 'day 1']
-# x2 = ['day 2', 'day 2', 'day 2', 'day 2', 'day 2', 'day 2']
-
-# fig = go.Figure()
-
-
-# fig.add_trace(go.Box(
-#     y=[0.2, 0.2, 0.6, 1.0, 0.5, 0.4],
-#     x=x1,
-#     name='kale1'
-# ))
-# fig.add_trace(go.Box(
-#     y=[0.2, 0.7, 0.9, 0.1, 0.5, 0.3],
-#     x=x2,
-#     name='kale2'
-# ))
-# fig.add_trace(go.Box(
-#     y=[0.6, 0.7, 0.3, 0.6, 0.0, 0.5, 0.7, 0.9, 0.5, 0.8, 0.7, 0.2],
-#     x=x1*2,
-#     name='radishes1'
-# ))
 
 data = files(TASK_NAME)
 testcase_list = sorted(data.get_pods_no())
-# print(pods_no)
-# podno_dict = {"pod0":}
+testcase_no = len(testcase_list)
+# fig = go.Figure()
+fig = make_subplots(
+    rows=2,
+    cols=testcase_no,
+    specs=[[{}]*testcase_no, [{"colspan": 3}]+[None]*(testcase_no-1)],
+    shared_xaxes=True,
+    shared_yaxes=True,
+    vertical_spacing=0.05,
+    horizontal_spacing=0.02,
+    subplot_titles=["2 pod", "4 pod", "6 pod"],
+)
+
+
 color_list = px.colors.qualitative.Bold
-for testcase in testcase_list:
-    print(testcase)
+is_showlegend = set()
+for tc_no, testcase in enumerate(testcase_list):
     filename_list = data.get_logs_name(testcase)
-    # print(filename_list)
+    y = []
     for pod_no, filename in enumerate(filename_list):
-        resp_time_list = get_response_time_list(filename)
-        # print([testcase] * len(resp_time_list))
+        row = 1
+        col = tc_no + 1
+        resp_time_list = get_response_time_from_file(filename)
+        y = resp_time_list
+        x = [f"testcase-{pod_no}"] * len(resp_time_list)
+        pod_name = f"pod{pod_no}"
         fig.add_trace(
             go.Box(
-                y=resp_time_list,
-                x=[testcase] * len(resp_time_list),
-                name=f"pod {pod_no}",
-                # boxpoints=False,
+                y=y,
+                x=x,
+                name=pod_name,
+                boxpoints=False,
                 marker_color=color_list[int(pod_no)],
                 # width=0.5
-                showlegend=False
-            )
+                legendgroup=f"pod{pod_no}",
+                showlegend=not (pod_name in is_showlegend),
+            ),
+            row=row,
+            col=col,
         )
+        is_showlegend.add(pod_name)
 
-fig.update_layout(
-    # height=600, width=300,
-    # boxgroupgap=0.2,
-    # boxgap=0.1,
-    boxmode='group'
-)
+for testcase in testcase_list:
+    filename_list = data.get_logs_name(testcase)
+    resp_time_list = get_response_time_from_filelist(filename_list)
+    fig.add_trace(
+        go.Box(
+            y=resp_time_list,
+            x=[testcase] * len(resp_time_list),
+            name=testcase,
+            boxmean='sd',
+            marker_color="deepskyblue",
+            # width=0.5
+            showlegend=False
+        ),
+        row=2,
+        col=1
+    )
+
+# fig.update_xaxes(visible=False)
+fig.update_xaxes(showticklabels=False)
+fig["layout"][f"yaxis1"]["title"] = "Response Time (second)"
+fig["layout"][f"yaxis4"]["title"] = "Response Time (second)"
+fig.update_layout(height=800, width=1300)
+
 # fig.write_html(OUTPUT)
 fig.show()
