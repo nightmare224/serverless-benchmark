5f5244036dd718ffa4fb8f135795da1936fb3f7e
Nicole Tai <nicole1999224@gmail.com>
2022-12-22 23:42:12 +0100

    output log and resource data

--- roles/trigger-openfaas-app/files/pattern4.sh
+++ roles/trigger-openfaas-app/files/pattern4.sh
@@ -2,4 +2,5 @@
 
 # twenty_per_three
 APPNAME=$1
-hey -t 0 -n 2 -c 2 http://127.0.0.1:8080/function/${APPNAME} > /tmp/out
+REQUEST_CONCURRENT=$2
+hey -t 0 -n ${REQUEST_CONCURRENT} -c ${REQUEST_CONCURRENT} http://127.0.0.1:8080/function/${APPNAME} > /tmp/out
